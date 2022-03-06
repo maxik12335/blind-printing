247cad90a48c0dd143941ce84ad4cfc8b0a571a1
Max <maxim.it@bk.ri>
2022-03-06 23:13:58 +0300

    no zoom _ 2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-document.addEventListener('touchmove', function (event) {
-    event = event.originalEvent || event;
-    if (event.scale !== 1) {
-        event.preventDefault();
-    }
-}, false);
-
 let main = document.querySelector('.main'),
     logo = document.querySelector('.header-heading')
 // main page
